f64f593edcd5b11f45743fcf06a40f3d071972ed
Lucas <lucasvddt2@yahoo.com.br>
2017-03-27 16:56:07 -0300

    Tela De Login e Tela de Cadastro Implementadas

--- app/src/main/java/com/trabalhodispositivosmoveisi/TelaCadastroActivity.java
+++ app/src/main/java/com/trabalhodispositivosmoveisi/TelaCadastroActivity.java
@@ -1,13 +1,41 @@
 package com.trabalhodispositivosmoveisi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class TelaCadastroActivity extends AppCompatActivity {
-
+    private EditText idCadastroEmailOuUsuario;
+    private EditText idCadastroSenha;
+    private Button idCadastro;
+    private Button idCancelar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_cadastro);
+        idCadastroEmailOuUsuario = (EditText) findViewById(R.id.editTextCadastroEmailOuUsuario);
+        idCadastroSenha = (EditText)findViewById(R.id.editTextCdastroSenha);
+        idCadastro = (Button)findViewById(R.id.idCadastro);
+        idCancelar = (Button)findViewById(R.id.buttonCancelar);
+        idCadastro = (Button) findViewById(R.id.idCadastro);
+
+        idCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        idCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent irParaTelaPerfilInformacoesPessoaisActivity = new Intent(TelaCadastroActivity.this, TelaPerfilInformacoesPessoaisActivity.class);
+                startActivity(irParaTelaPerfilInformacoesPessoaisActivity);
+            }
+        });
     }
 }
+
--- app/src/main/java/com/trabalhodispositivosmoveisi/TelaLoginActivity.java
+++ app/src/main/java/com/trabalhodispositivosmoveisi/TelaLoginActivity.java
@@ -1,13 +1,46 @@
 package com.trabalhodispositivosmoveisi;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class TelaLoginActivity extends AppCompatActivity {
+
+    private EditText editTextLoginEmailOuUsuario;
+    private EditText editTextLoginSenha;
+    private TextView textViewCriarConta;
+    private Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_login);
+
+        editTextLoginEmailOuUsuario = (EditText) findViewById(R.id.editTextLoginEmailOuUsuario);
+        editTextLoginSenha = (EditText) findViewById(R.id.editTextLoginSenha);
+
+        textViewCriarConta = (TextView) findViewById(R.id.textViewCriarConta);
+        textViewCriarConta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentParaTelaCadastroActivity = new Intent(TelaLoginActivity.this, TelaCadastroActivity.class);
+                startActivity(intentParaTelaCadastroActivity);
+            }
+        });
+
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent mIntent = new Intent(TelaLoginActivity.this, TelaPerfilActivity.class);
+                startActivity(mIntent);
+            }
+        });
     }
 }
