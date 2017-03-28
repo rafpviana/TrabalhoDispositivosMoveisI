637c947f17fed270a1352c0d580d5eac2aea3c8c
Isa Martins <isaura.ce@gmail.com>
2017-03-27 23:08:20 -0300

    TelaEditarPerfil

--- app/src/main/java/com/trabalhodispositivosmoveisi/TelaEditarPerfilActivity.java
+++ app/src/main/java/com/trabalhodispositivosmoveisi/TelaEditarPerfilActivity.java
@@ -1,13 +1,35 @@
 package com.trabalhodispositivosmoveisi;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TelaEditarPerfilActivity extends AppCompatActivity {
+
+    private Button buttonEditSalvar, buttonEditCancelar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_editar_perfil);
+
+        buttonEditCancelar = (Button) findViewById(R.id.buttonEditCancelar);
+        buttonEditSalvar = (Button) findViewById(R.id.buttonEditSalvar);
+
+        buttonEditCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        buttonEditSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
