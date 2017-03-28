7255189dda6539ef4ad7f56bab6c5246ce07e471
FernandoSouzaAds <l.fernanndo.rs@gmail.com>
2017-03-28 13:24:38 -0300

    Tela de informações físicas implementada.

--- app/src/main/java/com/trabalhodispositivosmoveisi/TelaPerfilInformacoesFisicasActivity.java
+++ app/src/main/java/com/trabalhodispositivosmoveisi/TelaPerfilInformacoesFisicasActivity.java
@@ -1,13 +1,28 @@
 package com.trabalhodispositivosmoveisi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TelaPerfilInformacoesFisicasActivity extends AppCompatActivity {
+
+    private Button b_id_avancar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_perfil_informacoes_fisicas);
+
+        b_id_avancar = (Button) findViewById(R.id.b_id_avancar);
+
+        b_id_avancar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newIntent = new Intent(TelaPerfilInformacoesFisicasActivity.this, TelaPerfilNivelAtividadeActivity.class);
+                startActivity(newIntent);
+            }
+        });
     }
 }
