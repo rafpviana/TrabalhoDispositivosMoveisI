c89a513e674369379a713a5d74f3523c2d164c57
rafael <rarpviana@gmail.com>
2017-03-28 08:59:39 -0300

    Implementação da tela de seleção do nível de atividade que o usuário deseja realizar.

--- app/src/main/java/com/trabalhodispositivosmoveisi/TelaPerfilNivelAtividadeActivity.java
+++ app/src/main/java/com/trabalhodispositivosmoveisi/TelaPerfilNivelAtividadeActivity.java
@@ -1,13 +1,27 @@
 package com.trabalhodispositivosmoveisi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TelaPerfilNivelAtividadeActivity extends AppCompatActivity {
+
+    private Button btnAvancar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_perfil_nivel_atividade);
+
+        btnAvancar = (Button) findViewById(R.id.btnAvancar);
+        btnAvancar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(TelaPerfilNivelAtividadeActivity.this, TelaPerfilActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
