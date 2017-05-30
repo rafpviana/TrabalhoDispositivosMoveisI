e31c8953ee2bfb34ff9d4585747fb9675be949b8
FernandoSouzaAds <l.fernanndo.rs@gmail.com>
2017-05-30 16:49:56 -0300

    Telas de Informações Físicas/Pessoais

--- app/src/main/java/com/trabalhodispositivosmoveisi/TelaPerfilInformacoesFisicasActivity.java
+++ app/src/main/java/com/trabalhodispositivosmoveisi/TelaPerfilInformacoesFisicasActivity.java
@@ -1,28 +1,87 @@
 package com.trabalhodispositivosmoveisi;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.ImageView;
+        import android.widget.TextView;
+        import android.widget.Toast;
+
+        import org.w3c.dom.Text;
 
 public class TelaPerfilInformacoesFisicasActivity extends AppCompatActivity {
 
-    private Button b_id_avancar;
+    private ImageView imageViewCadastroFotoPerfil;
+    private TextView textViewCadastroNome, textViewCadastroGenero, textViewCadastroIdade;
+    private EditText editTextCadastroPeso, editTextCadastroAltura;
+    private Button buttonCadastroAvancar;
+    private Usuario usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_perfil_informacoes_fisicas);
 
-        b_id_avancar = (Button) findViewById(R.id.b_id_avancar);
+        encontrarViews();
 
-        b_id_avancar.setOnClickListener(new View.OnClickListener() {
+        Intent intentDaTelaPerfilInformacoesPessoais = getIntent();
+        usuario = (Usuario) intentDaTelaPerfilInformacoesPessoais.getSerializableExtra("usuario");
+
+        if(usuario != null){
+//            Toast.makeText(this, "Id do usuário: "+usuario.getId(), Toast.LENGTH_SHORT).show();
+            preencherCabecalho();
+            configurarButtonCadastroAvancar();
+
+        }
+    }
+
+    private void encontrarViews() {
+        imageViewCadastroFotoPerfil = (ImageView) findViewById(R.id.imageview_cadastro_foto_perfil_2);
+        textViewCadastroNome = (TextView) findViewById(R.id.textview_cadastro_nome_1);
+        textViewCadastroGenero = (TextView) findViewById(R.id.textview_cadastro_genero_1);
+        textViewCadastroIdade = (TextView) findViewById(R.id.textview_cadastro_idade_1);
+        editTextCadastroPeso = (EditText) findViewById(R.id.edittext_cadastro_peso);
+        editTextCadastroAltura = (EditText) findViewById(R.id.edittext_cadastro_altura);
+        buttonCadastroAvancar = (Button) findViewById(R.id.button_cadastro_avancar_2);
+    }
+
+    private void preencherCabecalho(){
+        textViewCadastroNome.setText(usuario.getNome());
+        textViewCadastroGenero.setText(usuario.getGenero());
+        textViewCadastroIdade.setText(Integer.toString(usuario.getIdade())+" anos");
+    }
+
+    private void configurarButtonCadastroAvancar() {
+        buttonCadastroAvancar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent newIntent = new Intent(TelaPerfilInformacoesFisicasActivity.this, TelaPerfilNivelAtividadeActivity.class);
-                startActivity(newIntent);
+
+                if(editTextCadastroPeso.getText().toString() != null && !editTextCadastroPeso.getText().toString().isEmpty()
+                        && Float.parseFloat(editTextCadastroPeso.getText().toString()) > 0
+                        && editTextCadastroAltura.getText().toString() != null && !editTextCadastroAltura.getText().toString().isEmpty()
+                        && Float.parseFloat(editTextCadastroAltura.getText().toString()) > 0){
+
+                    usuario.setPeso(Float.parseFloat(editTextCadastroPeso.getText().toString()));
+                    usuario.setAltura(Float.parseFloat(editTextCadastroAltura.getText().toString()));
+
+                    irParaTelaPerfilNivelAtividade();
+
+                }
+                else{
+                    Toast.makeText(TelaPerfilInformacoesFisicasActivity.this, "Alguns campos estão inválidos ou não preenchidos!", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
     }
+
+    private void irParaTelaPerfilNivelAtividade(){
+        Intent intentParaTelaPerfilNivelAtividadeActivity = new Intent(TelaPerfilInformacoesFisicasActivity.this, TelaPerfilNivelAtividadeActivity.class);
+        intentParaTelaPerfilNivelAtividadeActivity.putExtra("usuario", usuario);
+        startActivity(intentParaTelaPerfilNivelAtividadeActivity);
+    }
+
 }
--- app/src/main/java/com/trabalhodispositivosmoveisi/TelaPerfilInformacoesPessoaisActivity.java
+++ app/src/main/java/com/trabalhodispositivosmoveisi/TelaPerfilInformacoesPessoaisActivity.java
@@ -1,36 +1,88 @@
 package com.trabalhodispositivosmoveisi;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.ImageView;
+        import android.widget.Toast;
 
 public class TelaPerfilInformacoesPessoaisActivity extends AppCompatActivity {
 
-    private ImageView imagemFoto;
-    private EditText textoEditavelNome,  textoEditavelGenero, textEditavelDataNascimento;
-    private Button botaoAvancar;
+    private ImageView imageViewCadastroFotoPerfil;
+    private EditText editTextCadastroNome, edittextCadastroGenero, editTextCadastroIdade;
+    private Button buttonCadastroAvancar;
+    private Usuario usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_perfil_informacoes_pessoais);
 
-        imagemFoto = (ImageView) findViewById(R.id.imagemFoto);
-        textoEditavelNome = (EditText) findViewById(R.id.textoEditavelNome);
-        textoEditavelGenero = (EditText) findViewById(R.id.textoEditavelGenero);
-        textEditavelDataNascimento = (EditText) findViewById(R.id.textoEditavelDataNascimento);
-        botaoAvancar = (Button) findViewById(R.id.botaoAvancar);
+        encontrarViews();
 
-        botaoAvancar.setOnClickListener(new View.OnClickListener() {
+        Intent intentDaTelaCadastro = getIntent();
+        usuario = (Usuario) intentDaTelaCadastro.getSerializableExtra("usuario");
+
+        if(usuario != null){
+//            Toast.makeText(this, "Id do usuário: "+usuario.getId(), Toast.LENGTH_SHORT).show();
+            configurarButtonCadastroAvancar();
+        }
+    }
+
+    private void encontrarViews() {
+        imageViewCadastroFotoPerfil = (ImageView) findViewById(R.id.imageview_cadastro_foto_perfil_1);
+        editTextCadastroNome = (EditText) findViewById(R.id.edittext_cadastro_nome);
+        edittextCadastroGenero = (EditText) findViewById(R.id.edittext_cadastro_genero);
+        editTextCadastroIdade = (EditText) findViewById(R.id.edittext_cadastro_idade);
+        buttonCadastroAvancar = (Button) findViewById(R.id.button_cadastro_avancar_1);
+    }
+
+    private void configurarButtonCadastroAvancar() {
+        buttonCadastroAvancar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(TelaPerfilInformacoesPessoaisActivity.this, TelaPerfilInformacoesFisicasActivity.class);
-                startActivity(intent);
+
+                if(editTextCadastroNome.getText().toString() != null && !editTextCadastroNome.getText().toString().isEmpty()
+                        && edittextCadastroGenero.getText().toString() != null && !edittextCadastroGenero.getText().toString().isEmpty()
+                        && editTextCadastroIdade.getText().toString() != null && !editTextCadastroIdade.getText().toString().isEmpty()){
+
+                    if(edittextCadastroGenero.getText().toString().equals("masculino")
+                            || edittextCadastroGenero.getText().toString().equals("Masculino")
+                            || edittextCadastroGenero.getText().toString().equals("MASCULINO")
+                            || edittextCadastroGenero.getText().toString().equals("feminino")
+                            || edittextCadastroGenero.getText().toString().equals("Feminino")
+                            || edittextCadastroGenero.getText().toString().equals("FEMININO")){
+
+                        if(Integer.parseInt(editTextCadastroIdade.getText().toString()) > 0){
+                            usuario.setNome(editTextCadastroNome.getText().toString());
+                            usuario.setIdade(Integer.parseInt(editTextCadastroIdade.getText().toString()));
+                            usuario.setGenero(edittextCadastroGenero.getText().toString());
+
+                            irParaTelaPerfilInformacoesFisicas();
+                        }
+                        else{
+                            Toast.makeText(TelaPerfilInformacoesPessoaisActivity.this, "Idade inválida!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else{
+                        Toast.makeText(TelaPerfilInformacoesPessoaisActivity.this, "Gênero inválido!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else{
+                    Toast.makeText(TelaPerfilInformacoesPessoaisActivity.this, "Alguns campos estão inválidos ou não preenchidos!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
+
+    private void irParaTelaPerfilInformacoesFisicas(){
+        Intent intentParaTelaPerfilInformacoesFisicasActivity = new Intent(TelaPerfilInformacoesPessoaisActivity.this, TelaPerfilInformacoesFisicasActivity.class);
+        intentParaTelaPerfilInformacoesFisicasActivity.putExtra("usuario", usuario);
+//        intentParaTelaPerfilInformacoesFisicasActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+//        finish();
+        startActivity(intentParaTelaPerfilInformacoesFisicasActivity);
     }
 }
