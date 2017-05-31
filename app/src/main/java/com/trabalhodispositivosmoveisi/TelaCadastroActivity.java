ff46ea8cd6715e5ca003907cc205230fa009a01c
lucasvddt <lucasvddt2@yahoo.com.br>
2017-05-31 11:22:38 -0300

    Telas Editar perfil e Tela cadastro atualizadas.

--- app/src/main/java/com/trabalhodispositivosmoveisi/TelaCadastroActivity.java
+++ app/src/main/java/com/trabalhodispositivosmoveisi/TelaCadastroActivity.java
@@ -6,37 +6,132 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class TelaCadastroActivity extends AppCompatActivity {
-    private EditText idCadastroEmailOuUsuario;
-    private EditText idCadastroSenha;
-    private Button idCadastro;
-    private Button idCancelar;
+
+    private EditText editTextCadastroEmail;
+    private EditText editTextCadastroSenha;
+    private EditText editTextCadastroConfirmacaoSenha;
+    private Button buttonCadastrar;
+    private Button buttonCancelarCadastro;
+    private Usuario usuario;
+    private Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_cadastro);
-        idCadastroEmailOuUsuario = (EditText) findViewById(R.id.editTextCadastroEmailOuUsuario);
-        idCadastroSenha = (EditText)findViewById(R.id.editTextCdastroSenha);
-        idCadastro = (Button)findViewById(R.id.idCadastro);
-        idCancelar = (Button)findViewById(R.id.buttonCancelar);
-        idCadastro = (Button) findViewById(R.id.idCadastro);
 
-        idCancelar.setOnClickListener(new View.OnClickListener() {
+        Realm.init(this);
+        realm = Realm.getDefaultInstance();
+
+        encontrarViews();
+
+        configurarButtonCancelarCadastro();
+
+        configurarButtonCadastrar();
+    }
+
+    private void encontrarViews() {
+        editTextCadastroEmail = (EditText) findViewById(R.id.edittext_cadastro_email);
+        editTextCadastroSenha = (EditText)findViewById(R.id.edittext_cadastro_senha);
+        editTextCadastroConfirmacaoSenha = (EditText) findViewById(R.id.editext_cadastro_confirmacao_senha);
+        buttonCadastrar = (Button)findViewById(R.id.button_cadastrar);
+        buttonCancelarCadastro = (Button)findViewById(R.id.button_cancelar_cadastro);
+    }
+
+    private void configurarButtonCancelarCadastro() {
+        buttonCancelarCadastro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
-        idCadastro.setOnClickListener(new View.OnClickListener() {
+    }
+
+    private void configurarButtonCadastrar() {
+        buttonCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent irParaTelaPerfilInformacoesPessoaisActivity = new Intent(TelaCadastroActivity.this, TelaPerfilInformacoesPessoaisActivity.class);
-                startActivity(irParaTelaPerfilInformacoesPessoaisActivity);
+                if(editTextCadastroEmail.getText().toString() != null && !editTextCadastroEmail.getText().toString().isEmpty()
+                        && editTextCadastroSenha.getText().toString() != null && !editTextCadastroSenha.getText().toString().isEmpty()
+                        && editTextCadastroConfirmacaoSenha.getText().toString() != null && !editTextCadastroConfirmacaoSenha.getText().toString().isEmpty()){
+
+                    if(editTextCadastroSenha.getText().toString().equals(editTextCadastroConfirmacaoSenha.getText().toString())){
+                        if(editTextCadastroSenha.getText().toString().length() >= 6) {
+
+                            usuario = new Usuario();
+                            usuario.setEmail(editTextCadastroEmail.getText().toString());
+                            usuario.setSenha(editTextCadastroSenha.getText().toString());
+                            usuario.setId(obterNovoIdDeUsuario());
+
+                            if (!usuarioJaExiste()) {
+                                irParaTelaPerfilInformacoesPessoais();
+                            } else {
+                                Toast.makeText(TelaCadastroActivity.this, "O usuário já existe!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else{
+                            Toast.makeText(TelaCadastroActivity.this, "A senha deve ter no mínimo 6 digitos!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else{
+                        Toast.makeText(TelaCadastroActivity.this, "As senhas não correspondem!", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                else{
+                    Toast.makeText(TelaCadastroActivity.this, "Alguns campos estão inválidos ou não preenchidos!", Toast.LENGTH_SHORT).show();
+                }
             }
+
         });
+    }
+
+    private boolean usuarioJaExiste(){
+        Usuario usuarioTeste = realm.where(Usuario.class).equalTo("email", usuario.getEmail()).findFirst();
+        if(usuarioTeste  == null){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    private void irParaTelaPerfilInformacoesPessoais(){
+        Intent intentIrParaTelaPerfilInformacoesPessoaisActivity = new Intent(TelaCadastroActivity.this, TelaPerfilInformacoesPessoaisActivity.class);
+        intentIrParaTelaPerfilInformacoesPessoaisActivity.putExtra("usuario",usuario);
+//        intentIrParaTelaPerfilInformacoesPessoaisActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+//        finish();
+        startActivity(intentIrParaTelaPerfilInformacoesPessoaisActivity);
+    }
+
+    private int obterNovoIdDeUsuario(){
+        int maiorIdDeUsuario = 0;
+        RealmResults<Usuario> listaDeUsuariosNoRealm = realm.where(Usuario.class).findAll();
+        if(listaDeUsuariosNoRealm != null && !listaDeUsuariosNoRealm.isEmpty()){
+            for(Usuario usuarioTexte : listaDeUsuariosNoRealm){
+                if(usuarioTexte.getId() > maiorIdDeUsuario){
+                    maiorIdDeUsuario = usuarioTexte.getId();
+                }
+            }
+        }
+
+        int novoIdDeUsuario = maiorIdDeUsuario+1;
+
+        return novoIdDeUsuario;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //realm.deleteAll();
+        realm.close();
     }
 }
 
--- app/src/main/java/com/trabalhodispositivosmoveisi/TelaEditarPerfilActivity.java
+++ app/src/main/java/com/trabalhodispositivosmoveisi/TelaEditarPerfilActivity.java
@@ -1,33 +1,273 @@
 package com.trabalhodispositivosmoveisi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import io.realm.Realm;
 
 public class TelaEditarPerfilActivity extends AppCompatActivity {
 
-    private Button buttonEditSalvar, buttonEditCancelar;
+    //private ImageView imageViewEditarFotoPerfil;
+    private EditText editTextEditarNome, editTextEditarGenero, editTextEditarIdade, editTextEditarPeso, editTextEditarAltura, editTextEditarEmail, editTextEditarAntigaSenha, editTextEditarNovaSenha, editTextEditarConfirmarNovaSenha;
+    private Button buttonEditarSalvar, buttonEditarCancelar;
+    private Spinner spinnerNivelAtividade;
+    private Usuario usuario;
+    private Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_editar_perfil);
 
-        buttonEditCancelar = (Button) findViewById(R.id.buttonEditCancelar);
-        buttonEditSalvar = (Button) findViewById(R.id.buttonEditSalvar);
+        Realm.init(this);
+        realm = Realm.getDefaultInstance();
 
-        buttonEditCancelar.setOnClickListener(new View.OnClickListener() {
+        encontrarViewa();
+
+        Intent intentDaTelaPerfil = getIntent();
+        int idUsuario = intentDaTelaPerfil.getIntExtra("idUsuario", 0);
+
+        if(idUsuario != 0){
+            usuario = obterUsuarioDoRealmPeloId(idUsuario);
+
+            if(usuario != null){
+//                Toast.makeText(this, "Id do usuário: "+usuario.getId(), Toast.LENGTH_SHORT).show();
+                preencherDadosDoPerfilDoUsuario();
+                configurarButtonEditarCancelar();
+                configurarButtonEditarSalvar();
+            }
+            else{
+                Toast.makeText(this, "Usuário inválido!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void encontrarViewa(){
+        //imageViewEditarFotoPerfil = (ImageView) findViewById(R.id.imageview_editar_foto_perfil);
+        editTextEditarNome = (EditText) findViewById(R.id.edittext_editar_nome);
+        editTextEditarGenero = (EditText) findViewById(R.id.edittext_editar_genero);
+        editTextEditarIdade = (EditText) findViewById(R.id.edittext_editar_idade);
+        editTextEditarPeso = (EditText) findViewById(R.id.edittext_editar_peso);
+        editTextEditarAltura = (EditText) findViewById(R.id.edittext_editar_altura);
+        editTextEditarEmail = (EditText) findViewById(R.id.edittext_editar_email);
+        editTextEditarAntigaSenha = (EditText) findViewById(R.id.edittext_editar_antiga_senha);
+        editTextEditarNovaSenha = (EditText) findViewById(R.id.edittext_editar_nova_senha);
+        editTextEditarConfirmarNovaSenha = (EditText) findViewById(R.id.edittext_editar_confirmar_nova_senha);
+        buttonEditarCancelar = (Button) findViewById(R.id.button_editar_cancelar);
+        buttonEditarSalvar = (Button) findViewById(R.id.button_editar_salvar);
+        spinnerNivelAtividade = (Spinner) findViewById(R.id.spinner_nivel_atividade);
+    }
+
+    private Usuario obterUsuarioDoRealmPeloId(int idUsuario){
+        Usuario usuarioTeste = realm.where(Usuario.class).equalTo("id", idUsuario).findFirst();
+        return usuarioTeste;
+
+    }
+
+    private void preencherDadosDoPerfilDoUsuario(){
+        editTextEditarNome.setText(usuario.getNome());
+        editTextEditarGenero.setText(usuario.getGenero());
+        editTextEditarIdade.setText(Integer.toString(usuario.getIdade()));
+        editTextEditarPeso.setText(Float.toString(usuario.getPeso()));
+        editTextEditarAltura.setText(Float.toString(usuario.getAltura()));
+        editTextEditarEmail.setText(usuario.getEmail());
+        spinnerNivelAtividade.setSelection(usuario.getNivelAtividade()-1);
+    }
+
+    private void configurarButtonEditarCancelar(){
+        buttonEditarCancelar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+    }
 
-        buttonEditSalvar.setOnClickListener(new View.OnClickListener() {
+    private void configurarButtonEditarSalvar(){
+        buttonEditarSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+
+                boolean atualizarUsuario = false, atualizarNome = false, atualizarGenero = false, atualizarIdade = false,
+                        atualizarPeso = false, atualizarAltura = false, atualizarEmail = false, atualizarSenha = false,
+                        atualizarNivelAtividade = false;
+
+
+                String textoNome = editTextEditarNome.getText().toString();
+                if(textoNome != null && !textoNome.isEmpty()){
+                    if(!textoNome.equals(usuario.getNome())){
+                        atualizarUsuario = true;
+                        atualizarNome = true;
+                    }
+                }
+                else{
+                    Toast.makeText(TelaEditarPerfilActivity.this, "Nome inválido!", Toast.LENGTH_SHORT).show();
+                    atualizarUsuario = false;
+                }
+
+                String textoGenero = editTextEditarGenero.getText().toString();
+                if(textoGenero != null && !textoGenero.isEmpty()){
+                    if(textoGenero.equals("masculino") || textoGenero.equals("Masculino") || textoGenero.equals("MASCULINO")
+                            || textoGenero.equals("feminino") || textoGenero.equals("Feminino") || textoGenero.equals("FEMININO")){
+                        if(!textoGenero.equals(usuario.getGenero())){
+                            atualizarUsuario = true;
+                            atualizarGenero = true;
+                        }
+                    }
+                    else{
+                        Toast.makeText(TelaEditarPerfilActivity.this, "Gênero inválido!", Toast.LENGTH_SHORT).show();
+                        atualizarUsuario = false;
+                    }
+                }
+                else{
+                    Toast.makeText(TelaEditarPerfilActivity.this, "Gênero inválido!", Toast.LENGTH_SHORT).show();
+                    atualizarUsuario = false;
+                }
+
+                String textoIdade = editTextEditarIdade.getText().toString();
+                if(textoIdade != null && !textoIdade.isEmpty() && Integer.parseInt(textoIdade) > 0){
+                    if(Integer.parseInt(textoIdade) != usuario.getIdade()){
+                        atualizarUsuario = true;
+                        atualizarIdade = true;
+                    }
+                }
+                else{
+                    Toast.makeText(TelaEditarPerfilActivity.this, "Idade inválida!", Toast.LENGTH_SHORT).show();
+                    atualizarUsuario = false;
+                }
+
+
+                int opcaoNivelAtividade = spinnerNivelAtividade.getSelectedItemPosition()+1;
+                if(opcaoNivelAtividade != usuario.getNivelAtividade()){
+                    atualizarUsuario = true;
+                    atualizarNivelAtividade = true;
+                }
+
+                String textoPeso = editTextEditarPeso.getText().toString();
+                if(textoPeso != null && !textoPeso.isEmpty() && Float.parseFloat(textoPeso)>0){
+                    if(Float.parseFloat(textoPeso) != usuario.getPeso()){
+                        atualizarUsuario = true;
+                        atualizarPeso = true;
+                    }
+                }
+                else{
+                    Toast.makeText(TelaEditarPerfilActivity.this, "Peso invválido!", Toast.LENGTH_SHORT).show();
+                    atualizarUsuario = false;
+                }
+
+                String textoAltura = editTextEditarAltura.getText().toString();
+                if(textoAltura != null && !textoAltura.isEmpty() && Float.parseFloat(textoAltura) > 0){
+                    if(Float.parseFloat(textoAltura) != usuario.getAltura()){
+                        atualizarUsuario = true;
+                        atualizarAltura = true;
+                    }
+                }
+                else{
+                    Toast.makeText(TelaEditarPerfilActivity.this, "Altura inválida!", Toast.LENGTH_SHORT).show();
+                    atualizarUsuario = false;
+                }
+
+                String textoEmail = editTextEditarEmail.getText().toString();
+                if(textoEmail != null && !textoEmail.isEmpty()){
+                    if(!textoEmail.equals(usuario.getEmail())){
+                        atualizarUsuario = true;
+                        atualizarEmail = true;
+                    }
+                }
+                else{
+                    Toast.makeText(TelaEditarPerfilActivity.this, "Email inválido!", Toast.LENGTH_SHORT).show();
+                    atualizarUsuario = false;
+                }
+
+                String textoAntigaSenha = editTextEditarAntigaSenha.getText().toString();
+                String textoNovaSenha = editTextEditarNovaSenha.getText().toString();
+                String textoConfirmarNovaSenha = editTextEditarConfirmarNovaSenha.getText().toString();
+
+                if (textoNovaSenha != null && !textoNovaSenha.isEmpty()) {
+
+                    if (textoNovaSenha.length() >= 6) {
+                        if (textoConfirmarNovaSenha != null && !textoConfirmarNovaSenha.isEmpty()) {
+                            if (textoConfirmarNovaSenha.equals(textoNovaSenha)) {
+                                if (textoAntigaSenha != null && !textoAntigaSenha.isEmpty() && textoAntigaSenha.equals(usuario.getSenha())) {
+                                    if (!textoNovaSenha.equals(usuario.getSenha())) {
+                                        atualizarUsuario = true;
+                                        atualizarSenha = true;
+                                    }
+                                    else{
+                                        Toast.makeText(TelaEditarPerfilActivity.this, "Nova senha igual a antiga senha!", Toast.LENGTH_SHORT).show();
+                                        atualizarUsuario = false;
+                                    }
+                                } else {
+                                    Toast.makeText(TelaEditarPerfilActivity.this, "Antiga senha inválida!", Toast.LENGTH_SHORT).show();
+                                    atualizarUsuario = false;
+                                }
+                            } else {
+                                Toast.makeText(TelaEditarPerfilActivity.this, "Confirmação da nova senha não corresponde!", Toast.LENGTH_SHORT).show();
+                                atualizarUsuario = false;
+                            }
+                        } else {
+                            Toast.makeText(TelaEditarPerfilActivity.this, "Confirmação da nova senha inválida!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else{
+                        Toast.makeText(TelaEditarPerfilActivity.this, "A nova senha deve ter no mínimo 6 dígitos!", Toast.LENGTH_SHORT).show();
+                        atualizarUsuario = false;
+                    }
+                }
+
+                if(atualizarUsuario){
+
+                    realm.beginTransaction();
+
+                    if(atualizarNome){
+                        usuario.setNome(textoNome);
+                    }
+
+                    if(atualizarGenero){
+                        usuario.setGenero(textoGenero);
+                    }
+
+                    if(atualizarIdade){
+                        usuario.setIdade(Integer.parseInt(textoIdade));
+                    }
+
+                    if(atualizarNivelAtividade){
+                        usuario.setNivelAtividade(opcaoNivelAtividade);
+                    }
+
+                    if(atualizarPeso){
+                        usuario.setPeso(Float.parseFloat(textoPeso));
+                    }
+
+                    if(atualizarAltura){
+                        usuario.setAltura(Float.parseFloat(textoAltura));
+                    }
+
+                    if(atualizarEmail){
+                        usuario.setEmail(textoEmail);
+                    }
+
+                    if(atualizarSenha){
+                        usuario.setSenha(textoNovaSenha);
+                    }
+
+                    realm.commitTransaction();
+
+                    Toast.makeText(TelaEditarPerfilActivity.this, "Usuário editado com sucesso!", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                else{
+                    Toast.makeText(TelaEditarPerfilActivity.this, "Usuário não editado!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
